ea22a0bc2ceaebd40e09f58d574330b74f5fe00a
JQQQ <jiqiang90@hotmail.com>
2022-01-26 11:19:40 +1300

    Update

--- src/chaintypes.ts
+++ src/chaintypes.ts
@@ -1,3 +1,27 @@
-import { types } from '@metaverse-network-sdk/type-definitions';
+import type { OverrideBundleDefinition } from '@polkadot/types/types';
 
-export default { typesBundle: types };
+// structs need to be in order
+/* eslint-disable sort-keys */
+
+const definitions: OverrideBundleDefinition = {
+    types: [
+        {
+            // on all versions
+            minmax: [0, undefined],
+            types:
+                {
+                    CurrencyId: {
+                        _enum: [
+                            'MA'
+                        ]
+                    },
+                    CurrencyIdOf: 'CurrencyId',
+                    Amount: 'i128',
+                    AmountOf: 'Amount',
+                    AccountInfo: 'AccountInfoWithDualRefCount'
+                }
+        }
+    ]
+};
+
+export default { typesBundle: definitions };
