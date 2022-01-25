f0839983cb9206d0fbdcf2e2215fe96d57601b65
JQQQ <jiqiang90@hotmail.com>
2022-01-25 17:12:15 +1300

    Update

--- src/chaintypes.ts
+++ src/chaintypes.ts
@@ -1,33 +1,23 @@
 import type { OverrideBundleDefinition } from "@polkadot/types/types";
 
+// structs need to be in order
+/* eslint-disable sort-keys */
+
 const definitions: OverrideBundleDefinition = {
     types: [
         {
             // on all versions
             minmax: [0, undefined],
             types: {
-                Keys: "AccountId",
-                Address: "MultiAddress",
-                LookupSource: "MultiAddress",
-                AmountOf: "Amount",
                 Amount: "i128",
-                SmartContract: {
-                    _enum: {
-                        Evm: "H160",
-                        Wasm: "AccountId",
-                    },
-                },
-                EraStakingPoints: {
-                    total: "Balance",
-                    stakers: "BTreeMap<AccountId, Balance>",
-                    formerStakedEra: "EraIndex",
-                    claimedRewards: "Balance",
-                },
-                EraRewardAndStake: {
-                    rewards: "Balance",
-                    staked: "Balance",
-                },
-                EraIndex: "u32",
+                Keys: "SessionKeys4",
+                AmountOf: "Amount",
+                Balance: "u128",
+                Rate: "FixedU128",
+                Ratio: "FixedU128",
+                EcdsaSignature: "[u8; 65]",
+                EvmAddress: "H160",
+                EthereumTxHash: "H256",
             },
         },
     ],
