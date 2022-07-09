7d92140deca9c0fe758b103764751ad1b753513d
wbug <wbug@163.com>
2022-07-09 08:50:49 +0800

    Update mappingHandlers.ts

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,32 +1,20 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {StarterEntity} from "../types";
+import {SubstrateEvent} from "@subql/types";
+import {Account} from "../types";
+import {Transfer} from "../types";
 import {Balance} from "@polkadot/types/interfaces";
 
-
-export async function handleBlock(block: SubstrateBlock): Promise<void> {
-    //Create a new starterEntity with ID using block hash
-    let record = new StarterEntity(block.block.header.hash.toString());
-    //Record block number
-    record.field1 = block.block.header.number.toNumber();
-    await record.save();
-}
-
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, balance]}} = event;
-    //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.block.block.header.hash.toString());
-    record.field2 = account.toString();
-    //Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
-    await record.save();
-}
-
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
-    //Date type timestamp
-    record.field4 = extrinsic.block.timestamp;
-    //Boolean tyep
-    record.field5 = true;
+    logger.info(`handle event  at ${event} `);
+    const {event: {data: [account, dest, balance]}} = event;
+     //Create a new Account entity with ID using block hash
+    //let record = new Account(event.extrinsic.block.block.header.hash.toString());
+    let record = new Transfer(account.toString()+ "_"+ event.extrinsic.block.block.header.number.toNumber().toString());
+    // Assign the Polkadot address to the account field
+    record.from = account.toString();
+    record.to = dest.toString();
+    // Assign the balance to the balance field "type cast as Balance"
+    record.balance = (balance as Balance).toBigInt();
+    record.timestamp = event.extrinsic.block.timestamp;
     await record.save();
 }
 
